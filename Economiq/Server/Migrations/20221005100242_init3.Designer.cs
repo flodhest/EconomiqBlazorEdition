91e56479f47d9315b3337c727b9bfced36d5dde8
Eric Flodin <eric.flodin@studerande-skys.se>
2022-10-05 12:34:17 +0200

    font-weight, aligntment etc in mobile.

--- Economiq/Server/Migrations/20221005100242_init3.Designer.cs
+++ Economiq/Server/Migrations/20221005100242_init3.Designer.cs
@@ -4,6 +4,7 @@ using Economiq.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Economiq.Server.Migrations
 {
     [DbContext(typeof(EconomiqContext))]
-    partial class EconomiqContextModelSnapshot : ModelSnapshot
+    [Migration("20221005100242_init3")]
+    partial class init3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Economiq/Server/Migrations/20221005100242_init3.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Economiq.Server.Migrations
+{
+    public partial class init3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Expenses",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ExpenseDate" },
+                values: new object[] { new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7933), new DateTime(2022, 10, 5, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7884));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7887));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7890));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7892));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7894));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7406));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7683));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7686));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7689));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7691));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7693));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7696));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreationDate",
+                value: new DateTime(2022, 10, 5, 12, 2, 42, 323, DateTimeKind.Local).AddTicks(7698));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Expenses",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ExpenseDate" },
+                values: new object[] { new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6232), new DateTime(2022, 9, 19, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6195));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6198));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6200));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6201));
+
+            migrationBuilder.UpdateData(
+                table: "ExpensesCategory",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6203));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6022));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6055));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6058));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6061));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6063));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6065));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6067));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreationDate",
+                value: new DateTime(2022, 9, 19, 11, 20, 16, 149, DateTimeKind.Local).AddTicks(6069));
+        }
+    }
+}
